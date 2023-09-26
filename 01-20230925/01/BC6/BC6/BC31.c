fe45ef065a4868bba6865e8ae821ec704e5e024f
yangyuanyuan <1163293065@qq.com>
2023-09-26 23:17:22 +0800

    2023-9-26 polished 6_10 homework

---
+++ 01-20230925/01/BC6/BC6/BC31.c
@@ -0,0 +1,14 @@
+//描述
+//你的手机丢了，在屏幕上输出信息告诉大家。
+//输入描述：
+//无
+//输出描述：
+//I lost my cellphone!
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	printf("I lost my cellphone!");
+//	return 0;
+//}
