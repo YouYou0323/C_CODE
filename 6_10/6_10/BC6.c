99b2ceb99bda5cda2f7986c6a1beafe0dc0478a2
yangyuanyuan <1163293065@qq.com>
2023-09-29 14:05:55 +0800

    2023-09-29 polished 6_14 homework

--- 6_10/6_10/BC6.c
+++ 6_10/6_10/BC6.c
@@ -10,3 +10,23 @@
 //	printf("    *  *\n");
 //	return 0;
 //}
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	short* s;
+	s = &a;
+	a = 10;
+	char* c;
+	b = 'W';
+	c = &b;
+	int* i;
+	double* d;
+	printf("%d\n", sizeof(s));
+	printf("%d\n", sizeof(c));
+	printf("%d\n", sizeof(i));
+	printf("%d\n", sizeof(d));
+	return 0;
+} 
---
+++ 6_14/6_14/BC112.c
@@ -0,0 +1,17 @@
+//小乐乐最近接触了求和符号Σ，他想计算的结果。但是小乐乐很笨，请你帮助他解答。
+//输入描述：
+//输入一个正整数n(1 ≤ n ≤ 109)
+//输出描述：
+//输出一个值，为求和结果。
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int i, n;
+	long long sum = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		sum = sum + i;
+	printf("%lld", sum);
+	return 0;
+}
