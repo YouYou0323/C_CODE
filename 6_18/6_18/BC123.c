103f5295633c0d8b7d3dc899ae8d34970c66dca4
yangyuanyuan <1163293065@qq.com>
2023-09-29 14:07:10 +0800

    2023-09-29 polished 6_18 homework

---
+++ 6_18/6_18/BC123.c
@@ -0,0 +1,19 @@
+//小乐乐获得4个最大数，请帮他编程找到最大的数。
+//输入描述：
+//一行，4个整数，用空格分开。
+//输出描述：
+//一行，一个整数，为输入的4个整数中最大的整数。
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int i, a, max=0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		scanf("%d", &a);
+//		if (a > max)
+//			max = a;
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
---
+++ 6_18/6_18/BC47.c
@@ -0,0 +1,28 @@
+//KiKi想判断输入的字符是不是字母，请帮他编程实现。
+//输入描述：
+//多组输入，每一行输入一个字符。
+//输出描述：
+//针对每组输入，输出单独占一行，判断输入字符是否为字母，输出内容详见输出样例。
+//输入：
+//A
+//6
+//输出：
+//A is an alphabet.
+//6 is not an alphabet.
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char a;
+//	while (scanf("%c", &a) != EOF)
+//	{
+//		if (a != '\n') {
+//			if (a >= 'A' && a <= 'z')
+//				printf("%c is an alphabet\n", a);
+//			else
+//				printf("%c is not an alphabet\n", a);
+//		}
+//		
+//	}
+//	return 0;
+}
