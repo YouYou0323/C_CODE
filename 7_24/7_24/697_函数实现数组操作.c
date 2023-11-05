c9074f4411a3be5b7885bb23604663e0e36731e1
yangyuanyuan <1163293065@qq.com>
2023-11-05 21:16:30 +0800

    2023-11-05 polished 7_24 homework

--- 7_24/7_24/697_函数实现数组操作.c
+++ 7_24/7_24/697_函数实现数组操作.c
@@ -4,46 +4,46 @@
 //2.实现print()  打印数组的每个元素
 //3.实现reverse()  函数完成数组元素的逆置。
 //要求：自己设计以上函数的参数，返回值。
-//#include<stdio.h>
-//void init(int a[],int sz)
-//{
-//	int i;
-//	for (i = 0; i < sz; i++)
-//	{
-//		a[i] = i+1;
-//	}
-//}
-//void print(int a[], int sz)
-//{
-//	int i;
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("a[%d]=%d\n", i, a[i]);
-//	}
-//}
-//void reversr(int a[], int sz)
-//{
-//	int i;
-//	int* p = &a[0];
-//	int temp = 0;
-//	for (i = sz - 1; i >=sz/2;i--)
-//	{
-//		temp = *p;
-//		*p = a[i];
-//		a[i] = temp;
-//		p++;
-//	}
-//}
-//int main()
-//{
-//	int a[10];
-//	int sz = sizeof(a) / sizeof(a[0]);
-//	init(a,sz);
-//	printf("初始化：\n");
-//	print(a,sz);
-//
-//	reversr(a,sz);
-//	printf("逆置后：\n");
-//	print(a, sz);
-//	return 0;
-//}
+#include<stdio.h>
+void init(int a[],int sz)
+{
+	int i;
+	for (i = 0; i < sz; i++)
+	{
+		a[i] = 0;
+	}
+}
+void print(int a[], int sz)
+{
+	int i;
+	for (i = 0; i < sz; i++)
+	{
+		printf("a[%d]=%d\n", i, a[i]);
+	}
+}
+void reverse(int a[], int sz)
+{
+	int i;
+	int* p = &a[0];
+	int temp = 0;
+	for (i = sz - 1; i >=sz/2;i--)
+	{
+		temp = *p;
+		*p = a[i];
+		a[i] = temp;
+		p++;
+	}
+}
+int main()
+{
+	int a[10]={1,2,3,4,5,6,7,8,9,10};
+	int sz = sizeof(a) / sizeof(a[0]);
+	//init(a,sz);
+	//printf("初始化：\n");
+	print(a,sz);
+
+	reverse(a,sz);
+	printf("逆置后：\n");
+	print(a, sz);
+	return 0;
+}
