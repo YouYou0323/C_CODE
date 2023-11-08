196763099c8b6d9c94112c7b0382508680a828ee
yangyuanyuan <1163293065@qq.com>
2023-11-08 23:42:32 +0800

    2023-11-08 accomplished 7_31 homework

---
+++ 7_31_初阶结构体/7_31_初阶结构体/728_喝汽水问题.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以喝多少汽水（编程实现）。
+//#include<stdio.h>
+//int main()
+//{
+//	int total = 0;
+//	int empty = 0;
+//	int money = 20;
+//	int one = 1;
+//	int yushu = 0;
+//	empty = money / one;
+//	while (empty >=2)
+//	{
+//		total += empty;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d", total);
+//	return 0;
+//}
