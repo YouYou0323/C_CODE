d8f3d5f00e46135871f8c27e4b41c9dbc60a7fbc
yangyuanyuan <1163293065@qq.com>
2023-11-09 10:31:40 +0800

    2023-11-09 accomplished 08_01 homework

---
+++ 08_01_const修饰指针/08_01_const修饰指针/736_strcpy实现.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<assert.h>
+//void my_strcpy(char* dest,char const *src)
+//{
+//	assert(dest != NULL);
+//	do
+//	{
+//		*dest = *src;
+//		dest++;
+//		src++;
+//	} while (*src != '\0');
+//}
+//int main()
+//{
+//	char a[10];
+//	int i = 0;
+//	char b[] = "abcdef";
+//	my_strcpy(a, b);
+//	for (i = 0; i < 6; i++)
+//	{
+//		printf("%c", a[i]);
+//	}
+//	return 0;
+//}
---
+++ 08_01_const修饰指针/08_01_const修饰指针/737_strlen实现.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int my_strlen(const char* p)
+//{
+//	int count = 0;
+//	while (*p != '\0')
+//	{
+//		p++;
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	char s[] = "123456";
+//	printf("×Ö·û´®³¤¶È£º%d", my_strlen(s));
+//	return 0;
+//}
